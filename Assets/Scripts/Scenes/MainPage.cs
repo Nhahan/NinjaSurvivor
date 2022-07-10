e0390f90c9c460f9728636b7075cc2ab55e563fc
sun <sun@codebrick.co>
2022-07-10 22:43:41 +0900

    Main page efffect

--- Assets/Scripts/Scenes/MainPage.cs
+++ Assets/Scripts/Scenes/MainPage.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
@@ -7,12 +5,53 @@ using UnityEngine;
 
 public class MainPage : MonoBehaviour
 {
-    private List<TextMeshProUGUI> _titleTexts;
-    private List<TextMeshProUGUI> _menuTexts;
+    [SerializeField] private List<TextMeshProUGUI> titleTexts;
+    [SerializeField] private List<TextMeshProUGUI> menuTexts;
+
+    private float _liveTIme;
 
     private void Start()
     {
-        _titleTexts = transform.GetComponentsInChildren<Transform>().Select(child => child.GetComponent<TextMeshProUGUI>()).ToList();
-        _menuTexts = transform.GetComponentsInChildren<Transform>().Select(child => child.GetComponent<TextMeshProUGUI>()).ToList();
+        foreach (var text in titleTexts.Concat(menuTexts))
+        {
+            text.color = new Color(0, 0, 0, 0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _liveTIme += Time.fixedDeltaTime;
+
+        if (_liveTIme > 1.5f) return;
+        foreach (var text in titleTexts)
+        {
+            text.color = new Color(
+                text.color.r + Time.fixedDeltaTime, 
+                text.color.g + Time.fixedDeltaTime, 
+                text.color.b + Time.fixedDeltaTime, 
+                text.color.a + Time.fixedDeltaTime);
+        }
+        
+        for (var i = 0; i < menuTexts.Count; i++)
+        {
+            var text = menuTexts[i];
+            
+            if (i == 0) 
+            {
+                text.color = new Color(
+                    text.color.r + Time.fixedDeltaTime, 
+                    text.color.g + Time.fixedDeltaTime, 
+                    text.color.b + Time.fixedDeltaTime, 
+                    text.color.a + Time.fixedDeltaTime);
+            }
+            else
+            {
+                text.color = new Color(
+                    text.color.r + Time.fixedDeltaTime, 
+                    text.color.g + Time.fixedDeltaTime, 
+                    text.color.b + Time.fixedDeltaTime, 
+                    text.color.a + Time.fixedDeltaTime / 20);
+            }
+        }
     }
 }
